b1d4a2b80355dced6121343a75a43215a38b4da7
Lcheck <znfhdl789@naver.com>
2023-01-05 21:40:22 +0900

    Feat MVC, API 방식의 요청/반환 예제

--- src/main/java/leh/lehspring/controller/LehController.java
+++ src/main/java/leh/lehspring/controller/LehController.java
@@ -4,6 +4,8 @@ package leh.lehspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //컨트롤러를 구분해주는 어노테이션
 
@@ -17,5 +19,39 @@ public class LehController {
             return "hello";
 
         }
+
+        @GetMapping("hello-mvc")
+        public String helloMvc (@RequestParam("name") String name, Model model){ //변수를 사용자의 파라미터로 받기
+
+            model.addAttribute("name",name);
+            return "hello-template";
+        }
+
+        @GetMapping("hello-string")
+        @ResponseBody
+        public String helloString(@RequestParam("name") String name){
+
+            return "hello"+name;
+        }
+
+        @GetMapping("hello-api")
+        @ResponseBody
+        public Hello helloApi(@RequestParam("name") String name){
+            Hello hello = new Hello();
+            hello.setName(name);
+            return hello;
+        }
+
+        static class Hello{
+
+            private String name;
+            public String getName(){
+                return name;
+            }
+            public void setName(String name){
+                this.name = name;
+            }
+        }
+
 
 }
